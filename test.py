6cc56abcfa5e0b9e6b6955532a3d75076d12bea6
Grischa Meyer <grischa@gmail.com>
2015-12-04 11:07:13 +1100

    testing includes pylint

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tardis.test_settings")
@@ -10,3 +11,14 @@ if __name__ == "__main__":
         execute_from_command_line(['./test.py', 'test'])
     else:
         execute_from_command_line(sys.argv)
+    pylint = subprocess.Popen(
+        ['pylint', '--rcfile=.pylintrc', '-j', '2', 'tardis'],
+        stdout=subprocess.PIPE)
+    pylint_output = True
+    for output in pylint.stdout:
+        if pylint_output:
+            print("pylint results")
+            pylint_output = False
+        print output,
+    if pylint_output:
+        print("pylint is happy")
